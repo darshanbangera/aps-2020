11bfb66e246d5b12338072d01a4b4118595b0007
DarshanBangera <34440319+darshanbangera@users.noreply.github.com>
2020-03-07 11:08:46 +0530

    Create 1.FenwickTree.cpp

---
+++ Code Library/1.FenwickTree.cpp
@@ -0,0 +1,37 @@
+#include<iostream>
+using namespace std
+
+int BIT[1000],a[1000];
+int n = 10;
+
+void update(int index, int value)
+{
+  for( ; index<=n ; index+=(index&-index))
+  {
+    BIT[i] += value;
+  }
+}
+int query(int i)
+{
+  int sum = 0;
+  for(;i>=1;i-=(i&-i))
+  {
+    sum = sum + BIT[i];
+  }
+  return sum;
+}
+
+void main()
+{
+  cin>>n;
+  int lower,upper,sum;
+  int i;
+  for(i = 0;i<=n;i++)
+  {
+    cin>>a[i];
+    update(i,a[i]);
+  }
+  
+  cin>>lower>>upper;
+  sum = query(upper) - query(lower);
+}
