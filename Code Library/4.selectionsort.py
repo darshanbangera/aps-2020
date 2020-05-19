ecb22c0accdcc7d46d638393da2fd8f72217a825
DarshanBangera <34440319+darshanbangera@users.noreply.github.com>
2020-05-19 19:19:33 +0530

    Create 4.selectionsort.py

---
+++ Code Library/4.selectionsort.py
@@ -0,0 +1,11 @@
+import sys 
+arr = list(map(int,input().split())
+for i in range(len(arr)):
+  min_idx = i 
+    for j in range(i+1, len(arr)): 
+        if arr[min_idx] > arr[j]: 
+            min_idx = j 
+  arr[i], arr[min_idx] = arr[min_idx], arr[i] 
+print ("Sorted array") 
+for i in range(len(arr)): 
+    print("%d" %arr[i]),  
