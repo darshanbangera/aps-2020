b76450bf743a0e07bebe660a7e9ac144f12d7086
DarshanBangera <34440319+darshanbangera@users.noreply.github.com>
2020-05-20 20:14:22 +0530

    Create perfect_number.py

---
+++ Code Library/perfect_number.py
@@ -0,0 +1,12 @@
+def isPerfect( n ): 
+	sum = 1
+	i = 2
+	while i * i <= n: 
+		if n % i == 0: 
+			sum = sum + i + n/i 
+		i += 1
+	
+	return (True if sum == n and n!=1 else False) 
+
+print(isPerfect(6))
+print(isPerfect(420))
