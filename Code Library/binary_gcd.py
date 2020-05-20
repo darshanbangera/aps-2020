602d238fb9caf8088093400c7238925499e55a60
DarshanBangera <34440319+darshanbangera@users.noreply.github.com>
2020-05-20 19:41:41 +0530

    Create binary_gcd.py

---
+++ Code Library/binary_gcd.py
@@ -0,0 +1,27 @@
+ef gcd(a, b) : 
+	
+	if (a == b) : 
+		return a 
+
+	if (a == 0) : 
+		return b 
+
+	if (b == 0) : 
+		return a 
+
+	if ((~a & 1)== 1 ) :	 
+		
+		if ((b & 1)== 1) :	 
+			return gcd(a >> 1, b) 
+		else : 
+			return (gcd(a >> 1, b >> 1) << 1) 
+	if ((~b & 1)== 1) : 
+		return gcd(a, b >> 1) 
+
+	if (a > b) : 
+		return gcd((a - b) >> 1, b) 
+
+	return gcd((b - a) >> 1, a) 
+
+a, b = 89612,620
+print(gcd(a, b)) 
