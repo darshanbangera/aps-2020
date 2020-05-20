3940e6064758655016715bb9d28e4a22d2b15931
DarshanBangera <34440319+darshanbangera@users.noreply.github.com>
2020-05-20 18:49:13 +0530

    Create plus_minus.py

---
+++ Code Library/hackerrank/plus_minus.py
@@ -0,0 +1,16 @@
+import math
+import os
+import random
+import re
+import sys
+
+# Complete the plusMinus function below.
+def plusMinus(arr):
+
+if __name__ == '__main__':
+    n = int(input())
+
+    arr = list(map(int, input().rstrip().split()))
+
+    plusMinus(arr)
+
