b1d9c7b29faffed9e1f767ac6e60e71ee0ab9822
DarshanBangera <34440319+darshanbangera@users.noreply.github.com>
2020-05-20 19:03:56 +0530

    Update plus_minus.py

--- Code Library/hackerrank/plus_minus.py
+++ Code Library/hackerrank/plus_minus.py
@@ -1,3 +1,5 @@
+#!/bin/python3
+
 import math
 import os
 import random
@@ -6,6 +8,19 @@ import sys
 
 # Complete the plusMinus function below.
 def plusMinus(arr):
+    c1 = 0
+    c2 = 0
+    c3 = 0
+    for i in range(len(arr)):
+        if arr[i] > 0:
+            c1 = c1 + 1
+        elif arr[i] < 0:
+            c2 = c2 + 1
+        else:
+            c3 = c3 + 1
+    print(round(c1/len(arr),6))
+    print(round(c2/len(arr),6))
+    print(round(c3/len(arr),6))
 
 if __name__ == '__main__':
     n = int(input())
@@ -13,4 +28,3 @@ if __name__ == '__main__':
     arr = list(map(int, input().rstrip().split()))
 
     plusMinus(arr)
-
