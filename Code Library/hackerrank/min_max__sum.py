c51d9226d8155bae5d576bbebc5a21f48b004d19
DarshanBangera <34440319+darshanbangera@users.noreply.github.com>
2020-05-20 18:50:06 +0530

    Create min_max__sum.py

---
+++ Code Library/hackerrank/min_max__sum.py
@@ -0,0 +1,18 @@
+
+import math
+import os
+import random
+import re
+import sys
+
+# Complete the miniMaxSum function below.
+def miniMaxSum(arr):
+    arr.sort()
+    min = arr[:len(arr)-1]
+    max = arr[1:]
+    print(sum(min),(sum(max)))
+
+if __name__ == '__main__':
+    arr = list(map(int, input().rstrip().split()))
+
+    miniMaxSum(arr)
